3ffd9611ee5ddfc107b90447c5322d6e35a36021
Alex Pilon <alp@alexpilon.ca>
2016-01-03 01:19:48 -0500

    Comment webcam arg out; functionality to add later.

--- voip.py
+++ voip.py
@@ -350,7 +350,7 @@ def argument_parser():
     ap.add_argument('--device', default=_DEFAULT_DEVICE, help='ffmpeg -devices')
     ap.add_argument('--microphone', default='default', help='ffmpeg -sources <device>')
     ap.add_argument('--speaker', default='default', help='ffmpeg -sinks <device>')
-    ap.add_argument('--webcam', default='/dev/video0', help='ffmpeg -sources')
+    #ap.add_argument('--webcam', default='/dev/video0', help='ffmpeg -sources')
 
     ap.add_argument('-p', '--public-address', nargs=2,
                     metavar=('HOST', 'PORT'))
